185bfd2551bb8287350749ee644524f893d4b9bd
IT Service <itservice@WynnSoft-Solution.com>
2025-02-20 10:43:10 +0700

    2-20/02

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        host: '192.168.1.124', // ตั้งค่าให้ใช้ IP ที่แชร์
-        strictPort: true,
-        cors: true, // ✅ เปิด CORS
-        hmr: {
-            protocol: 'ws', // ใช้ WebSocket แทน HTTP
-            host: '192.168.1.124',
-        }
-    },
+    // server: {
+    //     host: '192.168.1.124', // ตั้งค่าให้ใช้ IP ที่แชร์
+    //     strictPort: true,
+    //     cors: true, // ✅ เปิด CORS
+    //     hmr: {
+    //         protocol: 'ws', // ใช้ WebSocket แทน HTTP
+    //         host: '192.168.1.124',
+    //     }
+    // },
     plugins: [
         laravel(['resources/js/app.js', 'resources/css/app.css']),
     ],
